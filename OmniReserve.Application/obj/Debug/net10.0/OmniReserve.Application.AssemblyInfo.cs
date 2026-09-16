1ab1edc5cf875ac39baf5fff482fc066357d61d5
HenrCauich <alicauich2@gmail.com>
2026-09-16 13:43:23 -0600

    Tarea 4: implementacion de GetRoomByIdQuery, Handler y RoomResponseDto

--- OmniReserve.Application/obj/Debug/net10.0/OmniReserve.Application.AssemblyInfo.cs
+++ OmniReserve.Application/obj/Debug/net10.0/OmniReserve.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OmniReserve.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9928d280e688715fc3575c45a6b9bcfb18ed8ead")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e80b26ef87b48f12e4f5fbe7cc8170ca663b9e13")]
 [assembly: System.Reflection.AssemblyProductAttribute("OmniReserve.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OmniReserve.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
