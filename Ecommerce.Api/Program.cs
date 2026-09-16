a2aa905d618e22a06df3369d9560624573f223af
HenrCauich <alicauich2@gmail.com>
2026-09-16 11:51:39 -0600

    Tarea 1: configuré Swagger UI en el proyecto OmniReserve.Api para generar automáticamente la documentación interactiva de nuestros futuros endpoints.

--- Ecommerce.Api/Program.cs
+++ Ecommerce.Api/Program.cs
@@ -1,8 +1,21 @@
 var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddControllers();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
-app.MapGet("/api/status", () => {
-    return new { status = "Ecommerce API Online", version = "1.0.0" };
-});
 
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "OmniReserve.Api v1");
+    });
+}
+
+app.UseAuthorization();
+app.MapControllers();
 app.Run();
